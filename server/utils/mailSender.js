d64af6c016493c845bae8fb154127fdedbf1f6fe
mahesh24-s <maheshsonnari1234@gmail.com>
2026-03-11 21:18:29 +0530

    update nodemailer

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -8,6 +8,8 @@ const mailSender= async(email,title,body) =>{
         //transporter //shift this configuraion under config folder
         const transporter=nodemailer.createTransport({
             host:process.env.MAIL_HOST,
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
